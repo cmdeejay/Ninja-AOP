0f7042bb048e1712760f7350e7e93ca8dbbc0301
cmdeejay <charmyminchen@gmail.com>
2022-10-11 18:58:18 +0400

    NinjaFunctions class updated

--- utilities/functions.py
+++ utilities/functions.py
@@ -17,85 +17,85 @@ import json
 
 class NinjaFunctions:
     def __init__(self, email, client_id, first_name, last_name, login_session, status_label):
-        self.first_name = first_name.replace("\n", "")
-        self.last_name = last_name.replace("\n", "")
-        self.client_id = client_id.replace("\n", "")
-        self.email = email.replace("/n", "")
-        self.session = login_session
-        self.status_label = status_label
-        self.Url = Urls()
+        self._first_name = first_name.replace("\n", "")
+        self._last_name = last_name.replace("\n", "")
+        self._client_id = client_id.replace("\n", "")
+        self._email = email.replace("/n", "")
+        self._session = login_session
+        self._status_label = status_label
+        self._Url = Urls()
 
     def idDocuments(self):
         try:
-            os.makedirs(self.Url.os_path + self.email)
-            self.status_label.config(text="Creating client's folder...\n")
+            os.makedirs(self._Url.os_path + self._email)
+            self._status_label.config(text="Creating client's folder...\n")
         except OSError:
             tkinter.messagebox.showerror(title='Warning', message='The folder is already exist!')
-        self.status_label.config(text="Start downloading...\n")
-        url = self.Url.profile_url + self.client_id + "/show?_tab=_12"
-        r = self.session.get(url)
-        soup = BeautifulSoup(r.content, 'html5lib')
-        src = soup.find_all('img', attrs={'class': 'img-fluid width2'})
-        file_format = ["jpg", "pdf", "jpeg", "png", "JPG"]
+        self._status_label.config(text="Start downloading...\n")
+        _url = self._Url.profile_url + self._client_id + "/show?_tab=_12"
+        _r = self._session.get(_url)
+        _soup = BeautifulSoup(_r.content, 'html5lib')
+        _src = _soup.find_all('img', attrs={'class': 'img-fluid width2'})
+        _file_format = ["jpg", "pdf", "jpeg", "png", "JPG"]
         try:
-            idfront_src = soup.find('img', attrs={'class': 'img-fluid width2'}).get('src')
-            for source in src:
-                for f in file_format:
-                    if f in source.get('src'):
-                        file_name = f"{self.Url.os_path_f}/{self.email}/{self.first_name}{self.last_name}{int(time.time())}.{f}"
-                        urllib.request.urlretrieve(source.get('src'), file_name)
-                        os.startfile(file_name)
-            Uti = Utilities()
-            new_login_session = Uti.login()
-            generate_pdf = new_login_session.get(self.Url.pdfUrl(self.client_id))
-            r = new_login_session.get(url)
-            soup = BeautifulSoup(r.content, 'html5lib')
-            pdf = soup.find('span', attrs={'class': 'gen_pdf'}).find('a')['href']
-            self.status_label.config(text="All files download completed...\n")
-            return idfront_src, pdf
+            _idfront_src = _soup.find('img', attrs={'class': 'img-fluid width2'}).get('src')
+            for _source in _src:
+                for _f in _file_format:
+                    if _f in _source.get('src'):
+                        _file_name = f"{self._Url.os_path_f}/{self._email}/{self._first_name}{self._last_name}{int(time.time())}.{f}"
+                        urllib.request.urlretrieve(_source.get('src'), _file_name)
+                        os.startfile(_file_name)
+            _Uti = Utilities()
+            _new_login_session = _Uti.login()
+            _generate_pdf = _new_login_session.get(self._Url.pdfUrl(self._client_id))
+            _new_r = _new_login_session.get(_url)
+            _soup = BeautifulSoup(_new_r.content, 'html5lib')
+            _pdf = _soup.find('span', attrs={'class': 'gen_pdf'}).find('a')['href']
+            self._status_label.config(text="All files download completed...\n")
+            return _idfront_src, _pdf
         except AttributeError:
-            self.status_label.config(text="No files uploaded...\n")
+            self._status_label.config(text="No files uploaded...\n")
 
     def idTranslation(self):
-        Translator = google_translator()
-        Data_Generator = Tesseract_Translator()
+        _Translator = google_translator()
+        _Data_Generator = Tesseract_Translator()
         try:
-            os.makedirs(self.Url.os_path + self.email)
+            os.makedirs(self._Url.os_path + self._email)
         except OSError:
             tkinter.messagebox.showerror(title='Warning', message='The folder is already exist!')
 
-        format_path = "cot/COT_Format.pdf"
-        output_path = self.Url.os_path + self.email + "\\" + self.first_name + self.last_name + "_COT.pdf"
-        fillpdfs.get_form_fields(format_path)
-        return_data = Data_Generator.formatting()
-        form = {'Name and Job Position': 'Charmy Chen, Account Application Officer',
-                'Language': 'Chinese and English',
-                'ID Type': 'ID',
-                'ID Full Name': self.last_name + ' ' + self.first_name,
-                'Date of Birth': return_data['DOB'],
-                'Expiry Date': return_data['expiry_date'],
-                'ID Number': return_data['ID_number'],
-                'Document Type': 'ID',
-                'Residence Full Name': self.last_name + ' ' + self.first_name,
-                'Address': return_data['Address_en'],
-                'Issuing Date': return_data['issue_date']}
+        _format_path = "cot/COT_Format.pdf"
+        _output_path = self._Url.os_path + self._email + "\\" + self._first_name + self._last_name + "_COT.pdf"
+        fillpdfs.get_form_fields(_format_path)
+        _return_data = _Data_Generator.formatting()
+        _form = {'Name and Job Position': 'Charmy Chen, Account Application Officer',
+                 'Language': 'Chinese and English',
+                 'ID Type': 'ID',
+                 'ID Full Name': self._last_name + ' ' + self._first_name,
+                 'Date of Birth': _return_data['DOB'],
+                 'Expiry Date': _return_data['expiry_date'],
+                 'ID Number': _return_data['ID_number'],
+                 'Document Type': 'ID',
+                 'Residence Full Name': self._last_name + ' ' + self._first_name,
+                 'Address': _return_data['Address_en'],
+                 'Issuing Date': _return_data['issue_date']}
 
-        fillpdfs.write_fillable_pdf(format_path, output_path, form)
-        os.startfile(output_path)
+        fillpdfs.write_fillable_pdf(_format_path, _output_path, _form)
+        os.startfile(_output_path)
 
     def bo_check(self):
-        ser = Service('driver/chromedriver.exe')
-        normal_driver_option = webdriver.ChromeOptions()
-        normal_driver_option.add_experimental_option("detach", True)
-        normal_driver_option.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver = webdriver.Chrome(service=ser, options=normal_driver_option)
-        driver.maximize_window()
-        bo_url = Urls.profile_url + self.client_id + "/show"
-        driver.get(bo_url)
+        _ser = Service('driver/chromedriver.exe')
+        _normal_driver_option = webdriver.ChromeOptions()
+        _normal_driver_option.add_experimental_option("detach", True)
+        _normal_driver_option.add_experimental_option('excludeSwitches', ['enable-logging'])
+        _driver = webdriver.Chrome(service=_ser, options=_normal_driver_option)
+        _driver.maximize_window()
+        _bo_url = Urls.profile_url + self._client_id + "/show"
+        _driver.get(_bo_url)
         with open('restrict/bo_credentials.json', 'r') as fp:
-            login_data = json.load(fp)
-        username = driver.find_element(By.NAME, "admin_username")
-        username.send_keys(login_data["admin_username"])
-        password = driver.find_element(By.NAME, "admin_password")
-        password.send_keys(login_data["admin_password"])
-        driver.find_element(By.CLASS_NAME, "col-xs-4").click()
+            _login_data = json.load(fp)
+        _username = _driver.find_element(By.NAME, "admin_username")
+        _username.send_keys(_login_data["admin_username"])
+        password = _driver.find_element(By.NAME, "admin_password")
+        password.send_keys(_login_data["admin_password"])
+        _driver.find_element(By.CLASS_NAME, "col-xs-4").click()
